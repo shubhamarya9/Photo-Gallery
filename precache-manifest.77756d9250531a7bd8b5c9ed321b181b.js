a94cb1d7125f932eef2ca968c5d10fa337f031be
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2019-02-07 07:57:57 -0500

    Updates

---
+++ precache-manifest.77756d9250531a7bd8b5c9ed321b181b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae5ae173c016926ddb03",
+    "url": "/static/css/main.e318e1f4.chunk.css"
+  },
+  {
+    "revision": "ae5ae173c016926ddb03",
+    "url": "/static/js/main.ae5ae173.chunk.js"
+  },
+  {
+    "revision": "e90f1a7dcca7220164c6",
+    "url": "/static/js/1.e90f1a7d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6673d2a639827d63f93bf79e7c1d97f0",
+    "url": "/index.html"
+  }
+];
